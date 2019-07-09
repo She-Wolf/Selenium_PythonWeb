cd17e48223f11cb637060dfd8ae60b55b92dec5f
Your Name <you@example.com>
2019-07-09 16:05:28 +0300

    new steps for checkboxes and radiobuttons

--- lesson 2.1_step5.py
+++ lesson 2.1_step5.py
@@ -7,23 +7,26 @@ def calc(x):
     return str(math.log(abs(12 * math.sin(int(x)))))
 
 
-link = "http://suninjuly.github.io/math.html"
+link = "http://suninjuly.github.io/get_attribute.html"
 browser = webdriver.Chrome()
 browser.get(link)
 # open browser
 # count x
+x_element = browser.find_element_by_id('treasure')
+x = x_element.get_attribute('valuex')
 
-x_element = browser.find_element_by_id('input_value')
-x = x_element.text
 y = calc(x)
 # set formula
 fol = browser.find_element_by_id('answer')
 fol.send_keys(y)
-# set radio
-option1 = browser.find_element_by_css_selector("[for='robotsRule']")
-option1.click()
 
-browser.find_element_by_css_selector("[for='robotCheckbox']").click()
+
+browser.find_element_by_id('robotCheckbox').click()
+# set radio
+option1 = browser.find_element_by_id('robotsRule')
+# option1 = browser.find_element_by_css_selector("[for='robotsRule']")
+option1.click()
+# browser.find_element_by_css_selector("[for='robotCheckbox']").click()
 # sent form
 button = browser.find_element_by_css_selector("button.btn")
 button.click()
