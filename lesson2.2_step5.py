567f97e987dcf85a7a743b79e6f7518204b30145
Elena <shewolf664050@gmail.com>
2019-08-10 18:59:22 +0300

    scroll, scan value, checkboxes and radiobuttons

--- lesson2.2_step5.py
+++ lesson2.2_step5.py
@@ -1,4 +1,5 @@
 import math
+import time
 
 from selenium import webdriver
 
@@ -8,10 +9,11 @@ def main():
     link = " http://SunInJuly.github.io/execute_script.html"
     browser.get(link)
     # Считать значение для переменной x.
-    value = int(browser.find_element_by_id("input_value").text)
+    value = browser.find_element_by_id("input_value").text
     field = browser.find_element_by_id("answer")
     browser.execute_script("return arguments[0].scrollIntoView(true);", field)
-    field.send_keys(str(ln(value)))
+
+    field.send_keys(ln(value))
     # Выбрать чекбокс
     browser.find_element_by_css_selector("[for='robotCheckbox']").click()
     # Выбрать радиобаттон
@@ -19,11 +21,12 @@ def main():
     # Нажать кнопку
     button = browser.find_element_by_css_selector("button.btn")
     button.click()
+    time.sleep(100)
 
 
 # Посчитать математическую функцию от x.
 def ln(x):
-    math.log(abs(12 * math.sin(x)))
+    return str(math.log(abs(12 * math.sin(int(x)))))
 
 
 if __name__ == '__main__':
